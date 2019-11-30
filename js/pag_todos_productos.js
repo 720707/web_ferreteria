e003b83419a46b9c8576273e401fb45d43be6692
720707 <720707@unizar.es>
2019-11-30 12:17:27 +0100

    Update pag_todos_productos.js

--- js/pag_todos_productos.js
+++ js/pag_todos_productos.js
@@ -13,6 +13,20 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+var db = firebase.firestore();
+
+db.collection("users").add({
+    first: "Ada",
+    last: "Lovelace",
+    born: 1815
+})
+.then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id);
+})
+.catch(function(error) {
+    console.error("Error adding document: ", error);
+});
+
 window.onload = function(){
 	$("#manuales").fadeToggle();
 	$("#electricas").fadeToggle();
