6e477d911beb9140104e1b372185b1123c410450
720707 <720707@unizar.es>
2019-12-09 10:24:46 +0100

    Registrar usuario

--- js/registrar_usuario.js
+++ js/registrar_usuario.js
@@ -14,3 +14,17 @@ firebase.initializeApp(firebaseConfig);
 
 var db = firebase.firestore();
 
+
+$("#registrar").click(function(){
+	var email = $("#inputEmail").val();
+	var contraseña = $("#inputPassword").val();
+	console.log("Registrando "+contraseña+email)
+
+});
+
+firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // ...
+});
