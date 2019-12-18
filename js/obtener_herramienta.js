11de5d976584133d50be974ac46855220ebc1add
720707 <720707@unizar.es>
2019-12-18 15:55:18 +0100

    Update obtener_herramienta.js

--- js/obtener_herramienta.js
+++ js/obtener_herramienta.js
@@ -86,20 +86,37 @@ db.collection(coleccion).where("Nombre", "==", nom_producto)
     	console.log("Buscando herramienta" + nom_producto)
         querySnapshot.forEach(function(doc) {
         	console.log("Mostrando herramienta");
-            $("#producto").append("<hr class='featurette-divider'> " + 
-        	"<div class='row featurette'>" 
-        	+ "<div class='col-md-7 order-md-2 info_producto'>" +
-        	"<p><font size='3'>&nbsp <b>" + doc.data().Nombre + "</b> </p>"+
-        	"<p>Precio: &nbsp" + doc.data().Precio + "</p>"+ 
-        	"<p>Unidades Disponibles: &nbsp " + doc.data().Unidades +" </p>" + 
-        	"<p>Marca: &nbsp " + doc.data().Marca + "</p>" +
-        	"<p>Otras características: &nbsp Medidas : " + doc.data().Medidas + "</p>" +
-        	"<p>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Peso: " + doc.data().Peso + "</p>" +
-        	"<button class='btn btn-primary' type='button'><i class='fas fa-shopping-cart'></i>Comprar</button> "+
-        	"</div>" +
-        	"<div class='col-lg-4 col-md-6 col-sm-6 order-md-1'>" +
-        	"<img src=" + doc.data().Imagen + " width='300' height='275'>"
-        	+ "</div> </div>");
+        	if(coleccion == "herramientas_electricas" || coleccion == "herramientas_manuales"){
+        		$("#producto").append("<hr class='featurette-divider'> " + 
+	        	"<div class='row featurette'>" 
+	        	+ "<div class='col-md-7 order-md-2 info_producto'>" +
+	        	"<p><font size='3'>&nbsp <b>" + doc.data().Nombre + "</b> </p>"+
+	        	"<p>Precio: &nbsp" + doc.data().Precio + "</p>"+ 
+	        	"<p>Unidades Disponibles: &nbsp " + doc.data().Unidades +" </p>" + 
+	        	"<p>Marca: &nbsp " + doc.data().Marca + "</p>" +
+	        	"<p>Otras características: &nbsp Medidas : " + doc.data().Medidas + "</p>" +
+	        	"<p>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Peso: " + doc.data().Peso + "</p>" +
+	        	"<button class='btn btn-primary' type='button'><i class='fas fa-shopping-cart'></i>Comprar</button> "+
+	        	"</div>" +
+	        	"<div class='col-lg-4 col-md-6 col-sm-6 order-md-1'>" +
+	        	"<img src=" + doc.data().Imagen + " width='300' height='275'>"
+	        	+ "</div> </div>");
+        	}else{
+        		$("#producto").append("<hr class='featurette-divider'> " + 
+	        	"<div class='row featurette'>" 
+	        	+ "<div class='col-md-7 order-md-2 info_producto'>" +
+	        	"<p><font size='3'>&nbsp <b>" + doc.data().Nombre + "</b> </p>"+
+	        	"<p>Precio: &nbsp" + doc.data().Precio + "</p>"+ 
+	        	"<p>Unidades Disponibles: &nbsp " + doc.data().Unidades +" </p>" + 
+	        	"<p>Marca: &nbsp " + doc.data().Marca + "</p>" +
+	        	"<p>Otras características: &nbsp Potencia : " + doc.data().Potencia + "</p>" +
+	        	"<button class='btn btn-primary' type='button'><i class='fas fa-shopping-cart'></i>Comprar</button> "+
+	        	"</div>" +
+	        	"<div class='col-lg-4 col-md-6 col-sm-6 order-md-1'>" +
+	        	"<img src=" + doc.data().Imagen + " width='300' height='275'>"
+	        	+ "</div> </div>");
+        	}
+            
         	console.log("Mostrando herramienta");
     	});
     })
