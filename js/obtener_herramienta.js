b4bd3ccf6792c03f6e5a04f607cc0eb6ab5ca1ff
720707 <720707@unizar.es>
2020-02-01 17:29:08 +0100

    Update obtener_herramienta.js

--- js/obtener_herramienta.js
+++ js/obtener_herramienta.js
@@ -114,13 +114,12 @@ db.collection(coleccion).where("Nombre", "==", nom_producto)
 				 +"<div class='modal-dialog' role='document'>"+
 				    "<div class='modal-content'>"+
 				      "<div class='modal-header'>"+
-				        "<h5 class='modal-title' id='exampleModalLabel'>Modal title</h5>"+
+				        "<h5 class='modal-title' id='exampleModalLabel'>Comprar producto</h5>"+
 				        "<button type='button' class='close' data-dismiss='modal' aria-label='Close'>"+
 				          "<span aria-hidden='true'>&times;</span>"+
 				        "</button>"+
 				      "</div>"+
 				      "<div class='modal-body'>"+
-				      	//Poner lo de comprar
 				      "</div>"+
 				      "<div class='modal-footer'>"+
 				        "<button type='button' class='btn btn-secondary' data-dismiss='modal'>Cerrar</button>"+
