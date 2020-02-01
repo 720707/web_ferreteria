9a799d0d78d21a0ad81d12bf2e8c03066735d8a9
720707 <720707@unizar.es>
2020-02-01 16:09:01 +0100

    Ventana Emergente

--- js/obtener_herramienta.js
+++ js/obtener_herramienta.js
@@ -108,7 +108,7 @@ db.collection(coleccion).where("Nombre", "==", nom_producto)
 	        	"<div class='col-lg-4 col-md-6 col-sm-6 order-md-1'>" +
 	        	"<img src=" + doc.data().Imagen + " width='300' height='275'>"
 	        	+ "</div> </div>" +
-	            "<br><button id='boton_comprar' class='btn btn-primary' type='button'><i class='fas fa-shopping-cart'></i>&nbspComprar</button> ");
+	            "<br><button id='boton_comprar' href='#ventana1' class='btn btn-primary' data-toggle='modal' type='button'><i class='fas fa-shopping-cart'></i>&nbspComprar</button>");
         	}else{
         		$("#producto").append("<hr class='featurette-divider'> " + 
 	        	"<div class='row featurette'>" 
